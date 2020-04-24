e6484cec804e6324d721abd8c4954cb953e507fd
melisgb <gloriamelissa1@hotmail.com>
2020-04-24 17:52:28 +0100

    Passing info into Activity using Bundle.

--- app/src/main/java/com/ggonzales/zoogame/ViewAnimal.kt
+++ app/src/main/java/com/ggonzales/zoogame/ViewAnimal.kt
@@ -3,6 +3,7 @@ package com.ggonzales.zoogame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_view_animal.*
+import kotlinx.android.synthetic.main.animal_element.*
 
 class ViewAnimal : AppCompatActivity() {
 
@@ -10,8 +11,10 @@ class ViewAnimal : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_animal)
 
-        animalNameTView.text = intent.extras!!.get("name").toString()
-        animalDescTView.text = intent.extras!!.get("description").toString()
+        val bundle : Bundle = intent.extras!!
+        animalNameTView.text = bundle.getString("name")
+        animalDescTView.text = bundle.getString("description")
+        animalIButton.setImageResource(bundle.getInt("image"))
 
     }
 }
